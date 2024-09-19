182b724c06b847fe929b52ba64c7216697a53587
Pradeep-sahoo-kt <kt.pradeepsahoo@gmail.com>
2024-09-19 11:39:37 +0530

    all commit once

--- index.js
+++ index.js
@@ -2,41 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const escpos = require('escpos');
 require('escpos-network');  // Ensure you are requiring the network module
-
 const app = express();
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cors()); // Allow all origins
-
-
 // Mock Order Data
 const orderData = {
   kot: "KOT Order: Table 5\n1x Chicken Curry\n1x Rice",
   saveAndPrint: "Receipt\nTable 5\nTotal: $25.00"
 };
-
 // Fetch KOT Print Data
 app.get('/api/kot', (req, res) => {
   const printData = orderData.kot;
   res.json({ printData });
 });
-
 // Fetch Save & Print Data
 app.get('/api/receipt', (req, res) => {
   const printData = orderData.saveAndPrint;
+  console.log('Data to be printed:', printData)
   res.json({ printData });
 });
-
 // Print to Thermal Printer (ESC/POS)
 app.post('/api/print', async (req, res) => {
+  console.log('Received print job:', req.body);
   const { printerIp, printData } = req.body;
-
   if (!printerIp || !printData) {
     return res.status(400).json({ error: 'Printer IP and print data are required' });
   }
-
   const device = new escpos.Network(printerIp);
   const printer = new escpos.Printer(device);
-
   try {
     device.open(() => {
       printer
@@ -44,7 +37,6 @@ app.post('/api/print', async (req, res) => {
         .text(printData)
         .cut()
         .close();
-      
       res.json({ success: true });
     });
   } catch (error) {
@@ -52,7 +44,6 @@ app.post('/api/print', async (req, res) => {
     res.status(500).json({ error: 'Failed to print' });
   }
 });
-
 // Start server
 const port = 3000;
 app.listen(port, () => {
